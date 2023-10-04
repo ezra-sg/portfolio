ddd99ed1a97bcc37c6d05cab2afae2044b3e9a80
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-04 14:40:46 -0400

    Add shared styles package

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,3 +1,5 @@
+import 'ezra-sg-portfolio--styles/dist/index.css';
+
 export default function App() {
   return (
     <p>Hello world!</p>
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'ezra-sg-portfolio--styles/dist/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
