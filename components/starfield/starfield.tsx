559d0fff3ca68b20cf5afdd50da394f841f2e535
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-15 22:37:51 -0400

    refactor starfield

--- components/starfield/starfield.tsx
+++ components/starfield/starfield.tsx
@@ -8,6 +8,74 @@ import { StarData, VertexCache } from "@/components/starfield/starfield-types";
 import { initStars } from "@/components/starfield/starfield-utils";
 import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
 
+const vertexMemo: VertexCache = {};
+
+// create memoization table for storing calculated vertices
+function getDiamondVertices(size: number) {
+    // If vertices for this size have already been calculated, return them
+    if (vertexMemo[size]) {
+        return vertexMemo[size];
+    }
+
+    const vertices: Array<[number, number]> = [];
+    const innerSize = size * 0.5;
+    const outerSize = size;
+
+    for (let i = 0; i < 8; i++) {
+        const angle = Math.PI / 4 * i;
+        const length = i % 2 === 0 ? outerSize : innerSize;
+        const dx = Math.cos(angle) * length;
+        const dy = Math.sin(angle) * length;
+        vertices.push([dx, dy]);
+    }
+
+    // Store the calculated vertices in the memoization table
+    vertexMemo[size] = vertices;
+
+    return vertices;
+}
+
+function cancelAnimation(id: number | null) {
+    if (id !== null) {
+        window.cancelAnimationFrame(id);
+    }
+}
+
+function drawDotOnCanvas(ctx: CanvasRenderingContext2D, star: StarData) {
+    ctx.fillStyle = `#${star.color}`;
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function drawDiamondOnCanvas(ctx: CanvasRenderingContext2D, star: StarData) {
+    const cachedVertices = getDiamondVertices(star.size);
+    // vertices have been calculated relative to the origin, so we need to add the star's position to each vertex
+    const vertices = cachedVertices.map(vertex => [vertex[0] + star.x, vertex[1] + star.y]);
+
+    ctx.beginPath();
+
+    // Draw the star with curves
+    ctx.moveTo(vertices[0][0], vertices[0][1]);
+    for (let i = 0; i < vertices.length; i++) {
+        const x1 = vertices[i][0];
+        const y1 = vertices[i][1];
+        const x2 = vertices[(i + 1) % vertices.length][0];
+        const y2 = vertices[(i + 1) % vertices.length][1];
+        const cx = (x1 + x2) / 2;
+        const cy = (y1 + y2) / 2;
+        const cp1x = (cx + x1) / 2;
+        const cp1y = (cy + y1) / 2;
+        const cp2x = (cx + x2) / 2;
+        const cp2y = (cy + y2) / 2;
+
+        ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x2, y2);
+    }
+
+    ctx.fillStyle = `#${star.color}`;
+    ctx.fill();
+}
+
 export default function Starfield() {
     const prefersReducedMotion = usePrefersReducedMotion();
     const urlHasShowFps = useSearchParams().get('fps') === 'true';
@@ -16,7 +84,6 @@ export default function Starfield() {
     const [fps, setFps] = useState(0);
 
     const stars = useRef<StarData[]>([]);
-    const vertexMemo = useRef<VertexCache>({});
     const frameCount = useRef(0);
     const lastTimeFpsCounter = useRef(Date.now());
     const lastTimeDriftRate = useRef(Date.now());
@@ -25,31 +92,6 @@ export default function Starfield() {
 
     // useRef is used to create a stable reference to the drift() function so that it can be called recursively
     const driftFunctionRef = useRef<() => void>();
-
-    // create memoization table for storing calculated vertices
-    function getDiamondVertices (size: number) {
-        // If vertices for this size have already been calculated, return them
-        if (vertexMemo.current[size]) {
-            return vertexMemo.current[size];
-        }
-
-        const vertices: Array<[number, number]> = [];
-        const innerSize = size * 0.5;
-        const outerSize = size;
-
-        for (let i = 0; i < 8; i++) {
-            const angle = Math.PI / 4 * i;
-            const length = i % 2 === 0 ? outerSize : innerSize;
-            const dx = Math.cos(angle) * length;
-            const dy = Math.sin(angle) * length;
-            vertices.push([dx, dy]);
-        }
-
-        // Store the calculated vertices in the memoization table
-        vertexMemo.current[size] = vertices;
-
-        return vertices;
-    }
 
     function resetCanvas() {
         const canvas = canvasRef.current;
@@ -65,47 +107,6 @@ export default function Starfield() {
 
         stars.current = initStars(windowHeight, windowWidth);
     };
-
-    function cancelAnimation() {
-        if (animationFrameId.current) {
-            window.cancelAnimationFrame(animationFrameId.current);
-        }
-    }
-
-    function drawDotOnCanvas(ctx: CanvasRenderingContext2D, star: StarData) {
-        ctx.fillStyle = `#${star.color}`;
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.size, 0, 2 * Math.PI);
-        ctx.fill();
-    }
-
-    function drawDiamondOnCanvas(ctx: CanvasRenderingContext2D, star: StarData) {
-        const cachedVertices = getDiamondVertices(star.size);
-        // vertices have been calculated relative to the origin, so we need to add the star's position to each vertex
-        const vertices = cachedVertices.map(vertex => [vertex[0] + star.x, vertex[1] + star.y]);
-
-        ctx.beginPath();
-
-        // Draw the star with curves
-        ctx.moveTo(vertices[0][0], vertices[0][1]);
-        for (let i = 0; i < vertices.length; i++) {
-            const x1 = vertices[i][0];
-            const y1 = vertices[i][1];
-            const x2 = vertices[(i + 1) % vertices.length][0];
-            const y2 = vertices[(i + 1) % vertices.length][1];
-            const cx = (x1 + x2) / 2;
-            const cy = (y1 + y2) / 2;
-            const cp1x = (cx + x1) / 2;
-            const cp1y = (cy + y1) / 2;
-            const cp2x = (cx + x2) / 2;
-            const cp2y = (cy + y2) / 2;
-
-            ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x2, y2);
-        }
-
-        ctx.fillStyle = `#${star.color}`;
-        ctx.fill();
-    }
 
     function trackTime() {
         frameCount.current++;
@@ -186,7 +187,7 @@ export default function Starfield() {
 
     useEffect(() => {
         const resizeHandler = () => {
-            cancelAnimation();
+            cancelAnimation(animationFrameId.current);
             resetCanvas();
             driftFunctionRef.current?.();
         };
@@ -198,14 +199,14 @@ export default function Starfield() {
         window.addEventListener('focus', resizeHandlerDebounced);
 
         return () => {
-            cancelAnimation();
+            cancelAnimation(animationFrameId.current);
             window.removeEventListener('resize', resizeHandlerDebounced);
             window.removeEventListener('focus', resizeHandlerDebounced);
         };
     }, []);
 
     useEffect(() => {
-        cancelAnimation();
+        cancelAnimation(animationFrameId.current);
         if (!prefersReducedMotion) {
             driftFunctionRef.current?.();
         }
