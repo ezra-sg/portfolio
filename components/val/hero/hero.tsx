31abbc8f548851b99fc2d01b69643801421de91d
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-19 17:11:47 -0400

    fix hero responsiveness

--- components/val/hero/hero.tsx
+++ components/val/hero/hero.tsx
@@ -10,29 +10,29 @@ export default function Intro() {
     const { t } = useI18n();
 
     return (
-        <div className="relative w-full h-[100svh] md:flex md:flex-row-reverse md:gap-8 md:justify-between md:items-center xl:max-w-[1440px] xl:m-auto xl:px-12">
+        <div className="relative w-full h-[100svh] lg:flex lg:flex-row-reverse lg:gap-8 lg:justify-between lg:items-center xl:max-w-[1440px] xl:m-auto xl:px-12">
             <Image
                 src={img}
                 alt={t('hero.hero-image-alt')}
                 priority={true}
-                className="h-full w-full object-cover md:h-[100svh] xl:h-[80svh] md:w-auto"
+                className="h-full w-full object-cover lg:lg-[100svh] lg:w-auto xl:h-[80svh]"
             />
 
-            <div className="absolute md:static bottom-12 left-0 right-0 bg-amber-50 dark:bg-slate-950 py-4 md:w-full md:flex md:justify-center">
-                <div className="md:w-fit md:pl-4">
-                    <ReactMarkdown className="font-header text-5xl text-amber-900 dark:text-amber-200 text-center md:text-left">
+            <div className="absolute bottom-12 left-0 right-0 bg-amber-50 dark:bg-slate-950 py-4 lg:static lg:w-full lg:flex lg:justify-center">
+                <div className="lg:w-fit lg:pl-4">
+                    <ReactMarkdown className="font-header text-5xl text-amber-900 dark:text-amber-200 text-center lg:text-left">
                         {t('hero.hero-text')}
                     </ReactMarkdown>
 
-                    <ReactMarkdown className="mb-4 font-sans dark:text-amber-50 text-center font-semibold md:text-left">
+                    <ReactMarkdown className="mb-4 font-sans dark:text-amber-50 text-center font-semibold lg:text-left">
                         {t('hero.subtitle')}
                     </ReactMarkdown>
 
-                    <ReactMarkdown className="font-sans dark:text-amber-50 text-center md:text-left">
+                    <ReactMarkdown className="font-sans dark:text-amber-50 text-center lg:text-left">
                         {t('hero.author-text')}
                     </ReactMarkdown>
 
-                    <ReactMarkdown className="font-sans dark:text-amber-50 text-center md:text-left text-sm">
+                    <ReactMarkdown className="font-sans dark:text-amber-50 text-center lg:text-left text-sm">
                         {t('hero.interview-date')}
                     </ReactMarkdown>
                 </div>
