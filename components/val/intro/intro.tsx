4402507dc4494b60f22c165fe70dfc10582d18e6
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-19 17:20:22 -0400

    cleanup, add lorem ipsum

--- components/val/intro/intro.tsx
+++ components/val/intro/intro.tsx
@@ -1,17 +1,22 @@
+import ReactMarkdown from 'react-markdown';
+
 import { useI18n } from '@/hooks/useI18n';
+
+const firstParagraphClasses = {
+    general: 'font-serif dark:text-amber-50 mb-4',
+    firstLetter: 'first-letter:text-amber-900 first-letter:text-7xl first-letter:float-left first-letter:font-bold first-letter:mr-3 dark:first-letter:text-amber-200',
+};
 
 export default function Intro() {
     const { t } = useI18n();
 
-    return (<>
+    return (
         <div className="w-full bg-white dark:bg-slate-950 p-4 pt-12">
-            <p className="font-serif dark:text-amber-50 mb-4 first-letter:text-amber-900 first-letter:text-7xl first-letter:float-left first-letter:font-bold first-letter:mr-3 dark:first-letter:text-amber-200">
-                Valentín goes by <em>Val</em>, <em>Goose</em>, <em>Dad</em>, or <em>Doodles</em>, depending on who you ask.
-                The latter two are just for us kids to use. He really doesn&apos;t like being called <em>Doodles</em>.
-                He was born on May 23, 1950 in Corpus Christi, Texas.
-            </p>
+            <ReactMarkdown className={`${firstParagraphClasses.general} ${firstParagraphClasses.firstLetter}`}>
+                {t('intro.paragraph_1')}
+            </ReactMarkdown>
 
             <p className="font-serif dark:text-amber-50"></p>
         </div>
-    </>);
+    );
 }
