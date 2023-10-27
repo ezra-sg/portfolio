07ed75af3c93db14fc9e7ee3a3def27ae5d30c34
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-27 13:40:04 -0400

    make nav larger

--- components/val/progress-tracker/progress-tracker.tsx
+++ components/val/progress-tracker/progress-tracker.tsx
@@ -169,13 +169,13 @@ export default function ProgressTracker({
                 {buttons.map((button, index) => (
                     <button
                         key={`nav-button-${index}`}
-                        className={`${expanded ? 'h-6 w-6' : 'h-1.5 w-1.5'} rounded-full flex items-center justify-center bg-amber-50 border-[1px] border-amber-900 transition-all z-10`}
+                        className={`${expanded ? 'h-10 w-10 lg:w-8 lg:h-8' : 'h-1.5 w-1.5'} rounded-full flex items-center justify-center bg-amber-50 border-[1px] border-amber-900 transition-all z-10 hover:border-[2px]`}
                         aria-label={`${t('nav.go_to_section_label')} ${index + 1}`}
                         title={`${t('nav.go_to_section_label')} ${index + 1}`}
                         onClick={() => scrollTo({ top: button.ref.current!.offsetTop, behavior: 'instant' })}
                         onFocus={() => setExpanded(true)}
                     >
-                        {expanded && <span className="text-xs font-header text-amber-900">{button.label}</span>}
+                        {expanded && <span className="text-sm font-header text-amber-900">{button.label}</span>}
                     </button>
                 ))}
             </nav>
