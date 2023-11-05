899d569c68e2887caa1868e6d72d223f48125d79
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-11-05 17:11:21 -0500

    tweak spectrograph

--- hooks/useAudioContext.tsx
+++ hooks/useAudioContext.tsx
@@ -90,7 +90,7 @@ export function AudioProvider({ children }: { children: ReactNode }) {
             if (!audioAnalyser) {
                 analyzer.fftSize = 2048;
                 analyzer.minDecibels = -70;
-                analyzer.maxDecibels = -10;
+                analyzer.maxDecibels = -20;
                 setAudioAnalyser(analyzer);
             }
 
