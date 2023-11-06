07f8d305ba2c96203a767367f72d7e0167864767
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-11-06 15:02:00 -0500

    convert hero image to webp

--- components/val/hero/hero.tsx
+++ components/val/hero/hero.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 
-import img from '@/public/images/val--indian.jpg';
+import img from '@/public/images/val--indian.webp';
 
 import { useI18n } from '@/hooks/useI18n';
 
